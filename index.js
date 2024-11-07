e3f2b90a57610d52464fb6b46e2d65e58b8e8205
karljstn <47984349+karljstn@users.noreply.github.com>
2024-11-07 16:44:30 +0100

    feat: :sparkles: shareable prettier config

---
+++ index.js
@@ -0,0 +1,10 @@
+const config = {
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: es5,
+    printWidth: 80,
+    plugins: ["prettier-plugin-astro"]
+}
+
+export default config
